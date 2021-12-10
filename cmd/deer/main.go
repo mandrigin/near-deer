5e081065c1c76bbf1d0f8a93d7bba9eaf020f33f
Igor Mandrigin <i@mandrigin.ru>
2021-12-10 22:07:15 +0100

    add an actual check

--- cmd/deer/main.go
+++ cmd/deer/main.go
@@ -4,40 +4,67 @@ import (
 	"flag"
 	"fmt"
 	"os"
+
+	resty "github.com/go-resty/resty/v2"
 )
 
 var (
 	flagNetwork     = flag.String("network", "mainnet", "select network: mainnet or testnet")
-	flagNodeAddress = flag.String("node", "localhost:3030", "node to check against")
+	flagNodeAddress = flag.String("node", "http://localhost:3030", "node to check against")
 	flagThreshold   = flag.Int("threshold", 3, "health threshold, how many blocks behind is it okay to be for a local node")
 )
 
 func main() {
 	flag.Parse()
-	fmt.Println("hello", *flagNetwork, *flagNodeAddress)
+
+	fmt.Println("checking health", *flagNetwork, *flagNodeAddress)
+
 	sourceOfTruthBlock, err := getLatestBlockNumber(
 		getSourceOfTruthAddressForNetwork(*flagNetwork),
 	)
 	if err != nil {
-		fmt.Printf("error while getting source of truth block number: %v", err)
+		fmt.Printf("ERR: can't get source of truth block number: %v", err)
 		os.Exit(1)
 	}
 	localNodeBlock, err := getLatestBlockNumber(
 		*flagNodeAddress,
 	)
 	if err != nil {
-		fmt.Printf("error while getting local node block number: %v", err)
+		fmt.Printf("ERR: can't get local node block number: %v", err)
 		os.Exit(2)
 	}
 
-	if sourceOfTruthBlock-localNodeBlock > *flagThreshold { //negative is fine
-		fmt.Printf("error, the local node is too far away from the source of truth")
+	diff := sourceOfTruthBlock - localNodeBlock
+
+	if diff > *flagThreshold { //negative is fine
+		fmt.Println("ERR: The local node is too far away from the source of truth.", "difference", diff)
 		os.Exit(3)
 	}
+
+	fmt.Println("node is healthy", "difference", diff)
+}
+
+type StatusResult struct {
+	SyncInfo struct {
+		LatestBlockHeight int `json:"latest_block_height"`
+	} `json:"sync_info"`
 }
 
 func getLatestBlockNumber(address string) (int, error) {
-	return 0, nil
+	// Create a Resty Client
+	client := resty.New()
+
+	resp, err := client.R().SetResult(&StatusResult{}).Get(fmt.Sprintf("%s/status", address))
+	if err != nil {
+		return 0, err
+	}
+
+	result := resp.Result().(*StatusResult)
+	if result == nil {
+		return 0, nil
+	}
+
+	return result.SyncInfo.LatestBlockHeight, nil
 }
 
 func getSourceOfTruthAddressForNetwork(network string) string {
@@ -45,5 +72,5 @@ func getSourceOfTruthAddressForNetwork(network string) string {
 		return ""
 	}
 
-	return fmt.Sprintf("https://rpc.%s.near.org/status", network)
+	return fmt.Sprintf("https://rpc.%s.near.org", network)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/mandrigin/near-deer
 
 go 1.17
+
+require (
+	github.com/go-resty/resty/v2 v2.7.0 // indirect
+	golang.org/x/net v0.0.0-20211029224645-99673261e6eb // indirect
+)
